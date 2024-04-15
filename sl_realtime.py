c36012f4faf64828ab877856c8a43c1bef996f98
pablo <ekarlsson66@gmail.com>
2024-04-15 14:51:04 +0200

    slicing geodata

--- sl_realtime.py
+++ sl_realtime.py
@@ -1,9 +1,10 @@
 from google.transit import gtfs_realtime_pb2
 import requests
 
-key = "your-api-key"
+key = "b366e4ab8bc44017b4c65229323fa7b0"
 feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get("https://opendata.samtrafiken.se/gtfs-rt-sweden/{operator}/VehiclePositionsSweden.pb?key={key}")
+response = requests.get(
+    "https://opendata.samtrafiken.se/gtfs-rt-sweden/sl/VehiclePositionsSweden.pb?key=" + key)
 feed.ParseFromString(response.content)
 for entity in feed.entity:
     if entity.HasField('trip_update'):
